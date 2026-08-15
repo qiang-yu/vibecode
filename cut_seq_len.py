12e55693ec7f6275556b2455c3c252bbfc4f4ec8
qiang-yu <yuqiang0304@gmail.com>
2026-08-15 11:22:24 +0800

    add files

--- cut_seq_len.py
+++ cut_seq_len.py
@@ -29,7 +29,8 @@ ROLE_MAP_DEFAULT = {"human": "user", "gpt": "assistant", "system": "system", "to
 
 # Input files to process when --dataset is not provided. Modify these paths as needed.
 INPUT_FILES = [
-    "func-calling/Qwen3-8B/glaive-function-calling-5k-injected-1234-think-8b-clean-clean-tool_call_security-more-tools-clean.json",
+    "func-calling/Qwen3-8B/glaive-function-calling-5k-injected-simple-think-8b-clean-clean-tool_call_security-more-tools-clean-clean.json",
+    "func-calling/Qwen3-8B/glaive-function-calling-5k-injected-pretend-think-8b-clean-clean-tool_call_security-more-tools-clean-clean.json",    
 ]
 
 # Default tokenizer or model path. Modify this path as needed.
